b0aaa3d8266edc375bbb0625ac00c15544900903
GuYanSyue <88528224+GuYanSyue@users.noreply.github.com>
2022-10-25 23:41:29 +0800

    error fa

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 require('@nomiclabs/hardhat-waffle')
 require('@nomiclabs/hardhat-ethers')
 const API_URL = 'https://goerli.infura.io/v3/'
-const PRIVATE_KEY = '01e12448f4ab31997bb38a923c9c628e8a4dd61b4226403207a22d7161fa58a0'
+const PRIVATE_KEY = ''
 const PUBLIC_KEY = '0x3b0933b5EB572e5C1baC6f8D568B16EDDDFfB502'
 
 module.exports = {
